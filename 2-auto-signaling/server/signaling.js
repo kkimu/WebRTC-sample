9156109603adb71b11cacd0e9a8549320091076b
kkimu <kimura.kzm@gmail.com>
2019-11-09 20:00:32 +0900

    Add ws server

---
+++ 2-auto-signaling/server/signaling.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const WebSocketServer = require('ws').Server
+const port = 3001
+const wsServer = new WebSocketServer({ port })
+
+wsServer.on('connection', ws => {
+  ws.on('message', message => {
+    wsServer.clients.forEach(client => {
+      if (ws === client) {
+        console.log('skip render')
+      } else {
+        console.log(message)
+        client.send(message)
+      }
+    })
+  })
+})
